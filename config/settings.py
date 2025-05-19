3569b41ec142f223c84ad1b39a1e9e11ea885acb
kimjev-webdev <kimjevon.web@gmail.com>
2025-05-19 16:37:10 +0100

    Add Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,12 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    'theenglishstudiocorvetto.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
