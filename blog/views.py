3d394501f8a4df86c485a0896102e29e6dcd8a29
kimjev-webdev <kimjevon.web@gmail.com>
2025-06-08 16:29:10 +0100

    fix blog details mid size screen alignment polish

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,3 @@
-# blog/views.py
-
 from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
@@ -11,4 +9,20 @@ def blog_list(request):
 
 def blog_detail(request, slug):
     post = get_object_or_404(BlogPost, slug=slug, status='published')
-    return render(request, 'blog/blog_detail.html', {'post': post})
+
+    # Get previous and next based on publication date
+    previous_post = BlogPost.objects.filter(
+        published_at__gt=post.published_at,
+        status='published'
+    ).order_by('published_at').first()
+
+    next_post = BlogPost.objects.filter(
+        published_at__lt=post.published_at,
+        status='published'
+    ).order_by('-published_at').first()
+
+    return render(request, 'blog/blog_detail.html', {
+        'post': post,
+        'previous_post': next_post,
+        'next_post': previous_post,
+    })
