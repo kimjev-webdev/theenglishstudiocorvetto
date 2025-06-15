c06db4fb33b9a0af58d3d7b9ff26be799023c6eb
kimjev-webdev <kimjevon.web@gmail.com>
2025-06-15 20:47:29 +0000

    define contact path in schedule urls

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.calendar_view, name='schedule'),
+    path('contact/', views.contact_view, name='contact'),
     path(
         '<int:year>/<int:month>/',
         views.calendar_view,
