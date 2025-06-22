6ccbd7a03a2130ea56baf214a01ec957b88a4422
kimjev-webdev <kimjevon.web@gmail.com>
2025-06-22 09:51:27 +0100

    add crud to schedule urls.py

--- schedule/urls.py
+++ schedule/urls.py
@@ -8,4 +8,30 @@ urlpatterns = [
         views.calendar_view,
         name='calendar_by_date'
     ),
+
+    path('calendar/', views.calendar_view, name='calendar_view'),
+    path('schedule/', views.event_list_view, name='event_list'),
+    path('events/create/', views.create_event, name='create_event'),
+    path(
+        'events/<int:event_id>/edit/',
+        views.update_event,
+        name='update_event'
+    ),
+    path(
+        'events/<int:event_id>/delete/',
+        views.delete_event,
+        name='delete_event'
+    ),
+
+    path('classes/create/', views.create_class, name='create_class'),
+    path(
+        'classes/<int:class_id>/edit/',
+        views.update_class,
+        name='update_class'
+    ),
+    path(
+        'classes/<int:class_id>/delete/',
+        views.delete_class,
+        name='delete_class'
+    ),
 ]
