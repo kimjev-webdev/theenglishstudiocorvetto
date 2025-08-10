a580703df8ec579c29eeef41013e734b58ef7ec3
kimjev-webdev <kimjevon.web@gmail.com>
2025-08-10 11:43:19 +0100

    debug naming for blog portal

--- filesfordebug/filesfordebug/main/static/js/contact.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const thankYouModal = document.getElementById("thankYouModal");
-
-  if (thankYouModal && thankYouModal.dataset.show === "true") {
-    const modal = new bootstrap.Modal(thankYouModal);
-    modal.show();
-  }
-});
